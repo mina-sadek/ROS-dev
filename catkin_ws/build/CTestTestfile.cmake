b914a6b8aa3beda8aa11590c528456fc0e65b7c5
mina-sadek <sadek.mina@h-eng.helwan.edu.eg>
2017-04-30 03:12:12 +0200

    Add files via upload

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,32 @@
+# CMake generated Testfile for 
+# Source directory: /home/tornado/catkin_ws/src
+# Build directory: /home/tornado/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(CPP_1/CosPhi-ros_realtime/src/cosphi_ros_bridge_real_cpp)
+SUBDIRS(CPP_1/CosPhi-ros_gazebo/src/cosphi_ros_bridge_sim_cpp)
+SUBDIRS(CPP_1/cpp_1_description)
+SUBDIRS(CPP_1/cpp_1_gazebo)
+SUBDIRS(other/diff_drive)
+SUBDIRS(other/diff_drive_description)
+SUBDIRS(other/diff_drive_gazebo)
+SUBDIRS(diff_robot_ext_0/diff_robot_control)
+SUBDIRS(diff_robot_ext_0/diff_robot_description)
+SUBDIRS(maasndt/maasndt_control)
+SUBDIRS(maasndt/maasndt_description)
+SUBDIRS(mybot_control)
+SUBDIRS(mybot_description)
+SUBDIRS(robot_00)
+SUBDIRS(other/robot_1)
+SUBDIRS(gazebo_ros_demos/rrbot_control)
+SUBDIRS(gazebo_ros_demos/rrbot_description)
+SUBDIRS(gazebo_ros_demos/rrbot_gazebo)
+SUBDIRS(beginner_tutorials)
+SUBDIRS(ros_001)
+SUBDIRS(CPP_1/cpp_1_control)
+SUBDIRS(diff_robot_ext_0/diff_robot_gazebo)
+SUBDIRS(gazebo_ros_demos/custom_plugin_tutorial)
+SUBDIRS(maasndt/maasndt_gazebo)
+SUBDIRS(mybot_gazebo)
